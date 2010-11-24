06adbadd9425d532c037932e8e426a431d416626
Zanardo <zanardo@gmail.com>
2010-11-24 21:21:28 -0200

    #32: adicionando índices no schema

--- schema.sql
+++ schema.sql
@@ -8,6 +8,12 @@ CREATE TABLE tickets (
 	dateclosed datetime,
 	user text not null
 );
+CREATE INDEX tickets_dateclosed on tickets ( dateclosed desc );
+CREATE INDEX tickets_datecreated on tickets ( datecreated desc );
+CREATE INDEX tickets_datemodified on tickets ( datemodified desc );
+CREATE INDEX tickets_priority on tickets ( priority );
+CREATE INDEX tickets_status on tickets ( status );
+CREATE INDEX tickets_user on tickets ( user );
 
 CREATE TABLE comments ( 
 	id integer primary key not null,
@@ -16,6 +22,7 @@ CREATE TABLE comments (
 	user text not null,
 	comment text not null
 );
+CREATE INDEX comments_ticket_id on comments ( ticket_id );
 
 CREATE TABLE statustrack ( id integer primary key not null,
 	ticket_id integer not null references ticket ( id ),
@@ -23,11 +30,13 @@ CREATE TABLE statustrack ( id integer primary key not null,
 	user text not null,
 	status text not null
 );
+CREATE INDEX statustrack_ticket_id on statustrack ( ticket_id );
 
 CREATE TABLE tags (
 	ticket_id integer not null references ticket ( id ),
 	tag text not null
 );
+CREATE INDEX tags_ticket_id on tags ( ticket_id );
 
 CREATE TABLE timetrack (
 	id integer primary key not null,
@@ -36,6 +45,7 @@ CREATE TABLE timetrack (
 	user text not null,
 	minutes integer not null
 );
+CREATE INDEX timetrack_ticket_id on timetrack ( ticket_id );
 
 CREATE TABLE tagsdesc (
 	tag text not null primary key,
