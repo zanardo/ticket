a0955bee32dfd66021314112829acc6368fd8e65
Zanardo <zanardo@gmail.com>
2011-02-06 22:12:52 -0200

    atualizando schema para tabela contacts

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.11
+-- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: ticket
 -- ------------------------------------------------------
--- Server version	5.0.84
+-- Server version	5.1.49-1ubuntu8.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,15 +23,31 @@ DROP TABLE IF EXISTS `comments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comments` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `ticket_id` int(11) NOT NULL,
   `datecreated` datetime NOT NULL,
   `user` varchar(50) NOT NULL,
   `comment` text NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  KEY `comments_ticket_id` (`ticket_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+  KEY `comments_ticket_id` (`ticket_id`),
+  KEY `FK_comments_users` (`user`),
+  CONSTRAINT `FK_comments_users` FOREIGN KEY (`user`) REFERENCES `users` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contacts` (
+  `ticket_id` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  PRIMARY KEY (`ticket_id`,`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +63,7 @@ CREATE TABLE `search` (
   `tag` text,
   `user` text,
   `text` text,
-  PRIMARY KEY  (`docid`),
+  PRIMARY KEY (`docid`),
   FULLTEXT KEY `content` (`title`,`tag`,`user`,`text`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -60,15 +76,17 @@ DROP TABLE IF EXISTS `statustrack`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statustrack` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `ticket_id` int(11) NOT NULL,
   `datecreated` datetime NOT NULL,
   `user` varchar(50) NOT NULL,
   `status` text NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  KEY `statustrack_ticket_id` (`ticket_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+  KEY `statustrack_ticket_id` (`ticket_id`),
+  KEY `FK_statustrack_users` (`user`),
+  CONSTRAINT `FK_statustrack_users` FOREIGN KEY (`user`) REFERENCES `users` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,9 +113,9 @@ DROP TABLE IF EXISTS `tagsdesc`;
 CREATE TABLE `tagsdesc` (
   `tag` varchar(50) NOT NULL,
   `description` text,
-  `fgcolor` varchar(50) default NULL,
-  `bgcolor` varchar(50) default NULL,
-  PRIMARY KEY  (`tag`)
+  `fgcolor` varchar(50) DEFAULT NULL,
+  `bgcolor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`tag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -109,23 +127,24 @@ DROP TABLE IF EXISTS `tickets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tickets` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
-  `status` int(11) NOT NULL default '0',
-  `priority` int(11) NOT NULL default '3',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `priority` int(11) NOT NULL DEFAULT '3',
   `datecreated` datetime NOT NULL,
   `datemodified` datetime NOT NULL,
-  `dateclosed` datetime default NULL,
+  `dateclosed` datetime DEFAULT NULL,
   `user` varchar(50) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `tickets_dateclosed` (`dateclosed`),
   KEY `tickets_datecreated` (`datecreated`),
   KEY `tickets_datemodified` (`datemodified`),
   KEY `tickets_priority` (`priority`),
   KEY `tickets_status` (`status`),
-  KEY `tickets_user` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+  KEY `tickets_user` (`user`),
+  CONSTRAINT `FK_tickets_users` FOREIGN KEY (`user`) REFERENCES `users` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,15 +155,32 @@ DROP TABLE IF EXISTS `timetrack`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `timetrack` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `ticket_id` int(11) NOT NULL,
   `datecreated` datetime NOT NULL,
   `user` varchar(50) NOT NULL,
   `minutes` int(11) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  KEY `timetrack_ticket_id` (`ticket_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  KEY `timetrack_ticket_id` (`ticket_id`),
+  KEY `FK_timetrack_users` (`user`),
+  CONSTRAINT `FK_timetrack_users` FOREIGN KEY (`user`) REFERENCES `users` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user` varchar(50) NOT NULL,
+  `password` char(32) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -156,4 +192,4 @@ CREATE TABLE `timetrack` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-11-26 22:14:51
+-- Dump completed on 2011-02-06 22:12:24
