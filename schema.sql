efc9f9e7b7df6777d4be007c9a01fb163f2a6cc1
Antonio Zanardo <zanardo@gmail.com>
2012-09-22 12:54:32 -0300

    compatibilidade do schema com versão anterior

--- schema.sql
+++ schema.sql
@@ -1,61 +1,56 @@
 BEGIN;
 
-CREATE TABLE users (
-	username text NOT NULL PRIMARY KEY,
-	password text NOT NULL,
-	email text NOT NULL,
-	is_admin integer NOT NULL,
-	is_blocked integer NOT NULL
-);
-
-CREATE TABLE sessions (
-	id text NOT NULL PRIMARY KEY,
-	dtcreated text NOT NULL,
-	username text NOT NULL REFERENCES users ( username )
-);
-
 CREATE TABLE tickets (
 	id integer NOT NULL PRIMARY KEY,
 	title text NOT NULL,
-	status text NOT NULL,
-	dtcreated text NOT NULL,
-	dtmodified text NOT NULL,
-	dtclosed text,
-	usercreated text NOT NULL REFERENCES users ( username ),
-	usermodified text NOT NULL REFERENCES users ( username ),
-	userclosed text REFERENCES users ( username )
+	status integer NOT NULL DEFAULT ( 0 ),
+	priority integer NOT NULL DEFAULT ( 3 ),
+	datecreated text NOT NULL,
+	datemodified text NOT NULL,
+	dateclosed text,
+	user text NOT NULL
 );
 
 CREATE TABLE comments (
 	id integer NOT NULL PRIMARY KEY,
 	ticket_id integer NOT NULL REFERENCES tickets ( id ),
-	dtcreated text NOT NULL,
-	usercreated text NOT NULL REFERENCES users ( username ),
+	datecreated text NOT NULL,
+	user text NOT NULL,
 	comment text NOT NULL
 );
 
 CREATE TABLE tags (
 	id integer NOT NULL PRIMARY KEY,
 	ticket_id integer NOT NULL REFERENCES tickets ( id ),
+	tag text NOT NULL
+);
+
+CREATE TABLE tagsdesc (
 	tag text NOT NULL,
-	tagvalue text NOT NULL
+	description text NOT NULL,
+	fgcolor text NOT NULL,
+	bgcolor text NOT NULL
 );
 
 CREATE TABLE timetrack (
 	id integer NOT NULL PRIMARY KEY,
 	ticket_id integer NOT NULL REFERENCES tickets ( id ),
-	dtcreated text NOT NULL,
-	usercreated text NOT NULL REFERENCES users ( username ),
+	datecreated text NOT NULL,
+	user text NOT NULL,
 	minutes float NOT NULL
 );
 
-CREATE TABLE files (
+CREATE TABLE statustrack (
 	id integer NOT NULL PRIMARY KEY,
 	ticket_id integer NOT NULL REFERENCES tickets ( id ),
-	dtcreated text NOT NULL,
-	usercreated text NOT NULL REFERENCES users ( username ),
-	filename text NOT NULL,
-	filestoredname text NOT NULL
+	datecreated text NOT NULL,
+	user text NOT NULL,
+	status text NOT NULL
 );
 
-ROLLBACK;
+CREATE TABLE contacts (
+	ticket_id integer NOT NULL REFERENCES tickets ( id ),
+	email text NOT NULL
+);
+
+COMMIT;
