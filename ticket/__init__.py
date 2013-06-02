3a9fcd840e85100e9e2583597dbd26a51a86f105
Antonio Zanardo <zanardo@gmail.com>
2013-06-02 14:15:57 -0300

    não importa módulos desnecessários

--- ticket/__init__.py
+++ ticket/__init__.py
@@ -7,25 +7,6 @@
 # Para detalhes do licenciamento, ver COPYING na distribuição
 #
 
-import re
-import os
-import sys
-import zlib
-import time
-import bottle
-import getopt
-import random
-import getopt
-import os.path
-import sqlite3
-import datetime
-import mimetypes
-
-from uuid import uuid4
-from hashlib import sha1
-from bottle import route, request, run, view, response, static_file, \
-    redirect, local, get, post
-
 import config
 
 import ticket.db
