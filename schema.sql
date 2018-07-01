c27a0ac1abf8cf02bc2812345f7c749615b3b75e
Antonio Zanardo <zanardo@gmail.com>
2018-07-01 13:29:00 -0300

    ajustes no schema

--- schema.sql
+++ schema.sql
@@ -1,94 +1,91 @@
-BEGIN;
+begin;
 
-CREATE TABLE tickets (
-	id integer NOT NULL PRIMARY KEY,
-	title text NOT NULL,
-	status integer NOT NULL DEFAULT ( 0 ),
-	priority integer NOT NULL DEFAULT ( 3 ),
-	datecreated timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
-	datemodified timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
+create table users (
+	username text not null primary key,
+	password text not null,
+	is_admin int default (0),
+	email text,
+	name text
+);
+
+create table tickets (
+	id integer not null primary key,
+	title text not null,
+	status integer not null default (0) check (status between 0 and 1),
+	priority integer not null default (3) check (priority between 1 and 5),
+	datecreated timestamp not null default (datetime('now', 'localtime')),
+	datemodified timestamp not null default (datetime('now', 'localtime')),
 	datedue timestamp,
 	dateclosed timestamp,
-	user text NOT NULL,
-	admin_only integer NOT NULL DEFAULT ( 0 )
+	username text not null references users (username),
+	admin_only integer not null default (0)
 );
-CREATE INDEX idx_tickets_status ON tickets ( status );
-CREATE INDEX idx_tickets_datedue ON tickets ( datedue );
+create index idx_tickets_status on tickets (status);
 
-CREATE TABLE comments (
-	id integer NOT NULL PRIMARY KEY,
-	ticket_id integer NOT NULL,
-	datecreated timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
-	user text NOT NULL,
-	comment text NOT NULL
+create table comments (
+	id integer not null primary key,
+	ticket_id integer not null references tickets (id),
+	datecreated timestamp not null default (datetime('now', 'localtime')),
+	username text not null references users (username),
+	comment text not null
 );
-CREATE INDEX idx_comments_ticket_id ON comments ( ticket_id );
+create index idx_comments_ticket_id on comments (ticket_id);
 
-CREATE TABLE tags (
-	ticket_id integer NOT NULL,
-	tag text NOT NULL
+create table tags (
+	ticket_id integer not null references tickets (id),
+	tag text not null
 );
-CREATE INDEX idx_tags_ticket_id ON tags ( ticket_id );
+create index idx_tags_ticket_id on tags (ticket_id);
 
-CREATE TABLE tagsdesc (
-	tag text NOT NULL,
+create table tagsdesc (
+	tag text not null primary key,
 	description text,
 	fgcolor text,
 	bgcolor text
 );
 
-CREATE TABLE timetrack (
-	id integer NOT NULL PRIMARY KEY,
-	ticket_id integer NOT NULL,
-	datecreated timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
-	user text NOT NULL,
-	minutes float NOT NULL
+create table timetrack (
+	id integer not null primary key,
+	ticket_id integer not null references tickets (id),
+	datecreated timestamp not null default (datetime('now', 'localtime')),
+	username text not null references users (username),
+	minutes real not null
 );
-CREATE INDEX idx_timetrack_ticket_id ON timetrack ( ticket_id );
+create index idx_timetrack_ticket_id on timetrack (ticket_id);
 
-CREATE TABLE statustrack (
-	id integer NOT NULL PRIMARY KEY,
-	ticket_id integer NOT NULL,
-	datecreated timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
-	user text NOT NULL,
-	status text NOT NULL
+create table statustrack (
+	id integer not null primary key,
+	ticket_id integer not null references tickets (id),
+	datecreated timestamp not null default (datetime('now', 'localtime')),
+	username text not null references users (username),
+	status text not null
 );
-CREATE INDEX idx_statustrack_ticket_id ON statustrack ( ticket_id );
+create index idx_statustrack_ticket_id on statustrack (ticket_id);
 
-CREATE TABLE users (
-	username text NOT NULL PRIMARY KEY,
-	password text NOT NULL,
-	is_admin int,
-	email text,
-	name text
+create table sessions (
+	session_id text not null primary key,
+	date_login timestamp not null default (datetime('now', 'localtime')),
+	username text not null references users (username)
 );
 
-CREATE TABLE sessions (
-	session_id text NOT NULL PRIMARY KEY,
-	date_login timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
-	username text NOT NULL
+create table files (
+	id integer not null primary key,
+	ticket_id integer not null references tickets (id),
+	name text not null,
+	datecreated timestamp not null default (datetime('now', 'localtime')),
+	username text not null references users (username),
+	size integer not null check (size >= 0),
+	contents blob not null
+);
+create index idx_files_ticket_id on files (ticket_id);
+
+create table dependencies (
+	ticket_id integer not null references tickets (id),
+	blocks integer not null references tickets (id),
+	primary key(ticket_id, blocks)
 );
 
-CREATE TABLE files (
-	id integer NOT NULL PRIMARY KEY,
-	ticket_id integer NOT NULL,
-	name text NOT NULL,
-	datecreated timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
-	user text NOT NULL,
-	size integer NOT NULL,
-	contents blob NOT NULL
-);
-CREATE INDEX idx_files_ticket_id ON files ( ticket_id );
+insert into users (username, password, is_admin)
+values ('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1);
 
-CREATE TABLE dependencies (
-	ticket_id integer NOT NULL,
-	blocks integer NOT NULL,
-	PRIMARY KEY(ticket_id, blocks)
-);
-
-CREATE VIRTUAL TABLE search USING fts3 ( text );
-
-INSERT INTO users ( username, password, is_admin )
-VALUES ( 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1 );
-
-COMMIT;
+commit;
