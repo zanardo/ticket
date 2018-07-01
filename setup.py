7296ec58a4e4cf09f197c856da8a06ce287bba8d
Antonio Zanardo <zanardo@gmail.com>
2018-07-01 13:27:02 -0300

    ajusta dependências

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ setup(
     version=__version__,
     packages=["ticket"],
     install_requires=[
-        "zpgdb==0.4.2",
         "bottle==0.12.13",
-        "gunicorn",
-        "setproctitle",
     ],
 )
